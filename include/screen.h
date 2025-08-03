a1f51f264cf6096b6091a9a4f2c16ba4c679b215
_Encrypted <216389127+encrypted-official@users.noreply.github.com>
2025-08-04 00:57:17 +0530

    refactored major parts

--- include/screen.h
+++ include/screen.h
@@ -1,11 +1,32 @@
 #pragma once
 
-void Screen_StartMenu_Init(void);
-void Screen_StartMenu_Update(void);
-void Screen_StartMenu_Draw(void);
-void Screen_StartMenu_Unload(void);
+#include "raylib.h"
+#include "gui.h"
+#include <string.h>
 
-void Screen_Generator_Init(void);
-void Screen_Generator_Update(void);
-void Screen_Generator_Draw(void);
-void Screen_Generator_Unload(void);
+typedef void (*ScreenFunc)(void);
+
+void ScreenMenuBarInit(void);
+void ScreenMenuBarUpdate(void);
+void ScreenMenuBarDraw(void);
+void ScreenMenuBarUnload(void);
+
+void ScreenHomeInit(void);
+void ScreenHomeUpdate(void);
+void ScreenHomeDraw(void);
+void ScreenHomeUnload(void);
+
+void ScreenFileInit(void);
+void ScreenFileUpdate(void);
+void ScreenFileDraw(void);
+void ScreenFileUnload(void);
+
+void ScreenGenerateInit(void);
+void ScreenGenerateUpdate(void);
+void ScreenGenerateDraw(void);
+void ScreenGenerateUnload(void);
+
+extern ScreenFunc screenInitFuncs[];
+extern ScreenFunc screenUpdateFuncs[];
+extern ScreenFunc screenDrawFuncs[];
+extern ScreenFunc screenUnloadFuncs[];
